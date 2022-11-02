1254a78862f50ee2175d0cdb178a026e5af54396
Kimminwoo6039 <qkqhemforhs1@gmail.com>
2022-11-02 20:29:46 +0900

    2022/11/2 JPA
    기본키 매핑..

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -21,6 +21,9 @@ public class JpaMain {
 
         try {
 
+
+
+
             tx.commit();
         }catch (Exception e){
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -14,22 +14,21 @@ public class Member {
     @Column(name = "name") // DB 에는 name 인데 객체로는 username 으로 사용하고싶을때
     private String username;
 
-    private Integer age;
 
-    @Enumerated(EnumType.STRING) // enum 타입을 사용하고싶을때
-    private RoleType roleType;
 
-    @Temporal(TemporalType.TIMESTAMP) //날짜 타입을 사용하고싶을때
-    private Date createdDate;
 
-    @Temporal(TemporalType.TIMESTAMP) //날짜 타입을 사용하고싶을때
-    private Date lastModifiedDate;
 
-    @Lob // 문자타입 clob
-    private String description;
 
-    @Transient  // DB 를 만들지 않고 메모리에서 작동되게 하는 어노테이션
-    private int tte;
+
+
+
+
+
+
+
+
+
+
     public Member(){
 
     }
